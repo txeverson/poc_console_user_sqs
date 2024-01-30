7a4c41e297d6bcd01577430297c646aa6dd81f77
Everson Teixeira <everson.teixeira@dock.tech>
2024-01-30 12:39:43 -0300

    CORE-1922: fix sqs

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,32 +1,44 @@
 import { SQSQueue, Message } from '@main/sqs/queue'
-import express from 'express'
+import express, { Express } from 'express'
 import console from 'console'
 
 const queueUrl = 'https://sqs.us-east-2.amazonaws.com/245857777462/poc-importing-users-queue'
 const waitingForMessages = 5
 const sqs = new SQSQueue(queueUrl)
 
-sqs.receiveMessageFromQueueLoop(waitingForMessages)
-
-
-// Express test
-let count = 0
 const app = express()
-
 app.use(express.json())
-app.get('/queue/send', (req, res) => {
-  count++
 
-  const data: Message = {
-    Id: count,
-    Data: `Esta é a mensagem de número: ${count}`
-  }
+async function routes (): Promise<Express> {
 
-  sqs.sendMessageToQueue(data)
+  let count = 0
 
-  res.send({ response: 'Mensagem enviada com sucesso!' })
-})
+  app.get('/queue/send', (req, res) => {
+    count++
 
-app.listen(3000, () => {
-  console.log(`For the UI, open http://localhost:3000`)
-})
+    const data: Message = {
+      Id: count,
+      Data: `Esta é a mensagem de número: ${count}`
+    }
+
+    sqs.sendMessageToQueue(data)
+
+    res.send({ response: 'Mensagem enviada com sucesso!' })
+  })
+
+  return app
+}
+
+async function main () {
+
+  const route = await routes()
+
+  route.listen(3000, () => {
+    console.log(`For the UI, open http://localhost:3000/queue/send`)
+  })
+
+  await sqs.receiveMessageFromQueueLoop(waitingForMessages)
+
+}
+
+main()
